3679a9fdca750f5961e38eadaa2f14c620f6945e
Ryan Vieira <ryanvsouza99@gmail.com>
2024-08-12 15:40:04 -0300

    feat(transaction, create-transaction, tsconfig): add a new mutation to create a transaction

--- src/Models/transaction.ts
+++ src/Models/transaction.ts
@@ -1,7 +1,7 @@
 import { Schema, model, Document } from "mongoose";
 import { IAccount } from "./account";
 
-interface ITransaction extends Document {
+export interface ITransaction extends Document {
   fromAccount: IAccount;
   toAccount: IAccount;
   amount: number;
@@ -9,8 +9,8 @@ interface ITransaction extends Document {
 }
 
 const transactionSchema = new Schema({
-  fromAccount: { type: Schema.Types.ObjectId, ref: "Account" },
-  toAccount: { type: Schema.Types.ObjectId, ref: "Account" },
+  fromAccount: { type: Schema.Types.ObjectId, ref: "Account", required: true },
+  toAccount: { type: Schema.Types.ObjectId, ref: "Account", required: true },
   amount: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
 });
