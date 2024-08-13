c8f2f512c5e526fe1d39c68000b339cc2fa67969
Ryan Vieira <ryanvsouza99@gmail.com>
2024-08-13 17:17:48 -0300

    feat(index.ts, package.json): change koa-graphql to graphql-http

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import Koa from "koa";
 import Router from "@koa/router";
-import { graphqlHTTP } from "koa-graphql";
+import { createHandler } from "graphql-http/lib/use/koa";
+import { schema } from "./graphql/schema";
+import koaPlayGround from "graphql-playground-middleware-koa";
+import mongoose from "mongoose";
 
 const app = new Koa();
 const router = new Router();
 
+mongoose.connect("mongodb://localhost:27017/bank");
+
 router.get("/", (ctx) => {
-  console.log("oi");
+  const routes = ["/graphql - GraphiQL", "/playground - GraphQL Playground"];
+
+  ctx.status = 200;
+
+  ctx.body = routes.join("\n");
 });
 
-router.all("/graphql", graphqlHTTP({ schema: null, graphiql: true }));
+router.all("/graphql", createHandler({ schema }));
+router.all("/playground", koaPlayGround({ endpoint: "/graphql" }));
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(4001, () => console.log("Server running on port 4001"));
