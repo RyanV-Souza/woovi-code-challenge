5875809824b962b2600e5fff8f2cd36a9b6d08c0
Ryan Vieira <ryanvsouza99@gmail.com>
2024-08-13 17:50:35 -0300

    feat(queries); remove show balance

--- src/graphql/queries/index.ts
+++ src/graphql/queries/index.ts
@@ -1,13 +1,11 @@
 import { GraphQLObjectType } from "graphql";
 import { getAccountByIdQuery } from "./get-account-by-id";
 import { getAllAccountsQuery } from "./get-all-accounts";
-import { showBalanceQuery } from "./show-balance";
 
 export const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     account: getAccountByIdQuery,
     accounts: getAllAccountsQuery,
-    balance: showBalanceQuery,
   },
 });
