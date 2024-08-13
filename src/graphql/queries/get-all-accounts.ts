aa7ebffa493a0b703a104afcc83ed315368f2c14
Ryan Vieira <ryanvsouza99@gmail.com>
2024-08-13 17:17:24 -0300

    fix(graphql queries): modify get all acounts to use new node interface

--- src/graphql/queries/get-all-accounts.ts
+++ src/graphql/queries/get-all-accounts.ts
@@ -1,13 +1,43 @@
-import { GraphQLList } from "graphql";
-import { AccountType } from "../types/account-type";
-import { getAll } from "@/services/account";
+import { GraphQLInt, GraphQLString } from "graphql";
+import { AccountConnection } from "../connections/account-connection";
+import { Account } from "@/Models/account";
 
 export const getAllAccountsQuery = {
-  type: new GraphQLList(AccountType),
-  args: {},
+  type: AccountConnection,
+  args: {
+    first: { type: GraphQLInt },
+    after: { type: GraphQLString },
+  },
   async resolve(parent, args) {
-    const accounts = await getAll();
+    const query = {};
 
-    return accounts;
+    if (args.after) {
+      query["_id"] = {
+        $gt: Buffer.from(args.after, "base64").toString("ascii"),
+      };
+    }
+
+    const accounts = await Account.find(query)
+      .sort({ createdAt: 1 })
+      .limit(args.first + 1);
+
+    const hasNextPage = accounts.length > args.first;
+
+    if (hasNextPage) {
+      accounts.pop();
+    }
+
+    const edges = accounts.map((account) => ({
+      cursor: Buffer.from(account.id.toString()).toString("base64"),
+      node: account,
+    }));
+
+    return {
+      edges,
+      pageInfo: {
+        endCursor: edges.length ? edges[edges.length - 1].cursor : null,
+        hasNextPage,
+      },
+    };
   },
 };
